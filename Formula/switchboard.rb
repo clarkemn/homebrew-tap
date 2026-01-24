1ea38a5ba17193201d61d506f99ceb8e4f4ea6a4
Matt Clarke <clarkemn@gmail.com>
2026-01-24 13:45:33 -0500

    fix(switchboard): restore version declaration in formula

--- Formula/switchboard.rb
+++ Formula/switchboard.rb
@@ -1,9 +1,9 @@
 class Switchboard < Formula
   desc "Native macOS GUI for Granted AWS profile management"
   homepage "https://github.com/clarkemn/switchboard"
-  version "1.0.0"
   url "https://github.com/clarkemn/switchboard/releases/download/v1.0.0/switchboard-1.0.0-darwin-arm64.tar.gz"
   sha256 "d83460543be4118155ec7f84dab4a040316ebd2b4a8f2e2b5e48bb5ff9d7cb7a"
+  version "1.0.0"
 
   def install
     prefix.install "Switchboard.app"
